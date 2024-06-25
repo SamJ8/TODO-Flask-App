a65ebf378c97d125399e9eb46e1c9a0df7e07d94
Sam Johnson <80408211+SamJ8@users.noreply.github.com>
2024-06-25 14:27:59 +0100

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from website import create_app
-
+app = create_app()
 if __name__=="__main__":
-    app = create_app()
+    
     app.run(debug=True, port=8000)
 
     #! This is where the actual running of the app occurs.
